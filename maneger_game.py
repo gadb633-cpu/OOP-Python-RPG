a5c8f896bd58658dfbcf52a5fdf307d9c09232f9
gad-biton <gadb633@gmail.com>
2026-07-27 14:48:28 +0300

    added class game_manager

---
+++ maneger_game.py
@@ -0,0 +1,14 @@
+class Game_manager:
+    def __init__(self):
+        self.playre = ""
+        self.maze = []
+        self.monster = ""
+    def create_playet():
+        pass
+    def create_monster():
+        pass
+    def create_maze():
+        pass
+    def Game_summary():
+        pass
+        

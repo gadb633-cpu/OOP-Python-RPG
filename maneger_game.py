2d39e7b04835cd6e78a4ee2f3717517d78945a60
Moshe Pfeffer <moishipfeffer@gmail.com>
2026-07-27 16:54:18 +0300

    get added

--- maneger_game.py
+++ maneger_game.py
@@ -9,10 +9,13 @@ class Game_manager:
         self.maze = None
     def create_player(self):
         self.player=Player(PLAYER_NAME,PLAYER_LIFE,PLAYER_STRENGH,PLAYER_AGILITY,PLAYER_LUCK)
+    def get_player(self):
         return self.player
     def create_maze(self):
         self.maze=Maze()
+    def get_maze(self):
         return self.maze
     def Game_summary():
+        pass
         
 game=Game_manager()

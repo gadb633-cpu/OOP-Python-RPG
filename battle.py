207b10fae53bdec5e2ab21a09f2b4732fbb7462c
Elyashiv <elyashiv988@gmail.com>
2026-07-27 15:50:11 +0300

    fixed errors

--- battle.py
+++ battle.py
@@ -8,17 +8,21 @@ class Battle:
         self.winner=None
 
     def choose_attacker(self):
-        if self.player.agility + self.player.luck > self.monster.agility + self.monster.luck:
+        if self.player.agility + self.player.luck >= self.monster.agility + self.monster.luck:
             self.attacker=self.player and self.defender=self.monster
         else:
-            self.attacker=self.monster and self.defender=self.monster
+            self.attacker=self.monster and self.defender=self.player
 
     def attack(self):
         return self.defender.life - self.defender.life
 
     def check_winner(self):
         if self.attack()<=0:
-            self.winner = self.attacker
+            self.winner = self.attacker.name
+            return self.winner
+
+        
+            
     
 
         

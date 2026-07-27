95417950598308ef3ec25e5fdfa621634621594f
Elyashiv <elyashiv988@gmail.com>
2026-07-28 01:24:56 +0300

    fixed battle file addad run func

--- battle.py
+++ battle.py
@@ -1,26 +1,44 @@
-
+from config import *
 class Battle:
     def __init__(self,player,monster):
         self.player=player
         self.monster=monster
         self.attacker=None
-        self.defender=None
-        self.winner=None
+        self.defander=None
 
-    def choose_attacker(self):
+    def first_attacker(self):
         if self.player.agility + self.player.luck >= self.monster.agility + self.monster.luck:
-            self.attacker=self.player and self.defender=self.monster
+            self.attacker=self.player
+            self.defander=self.monster
         else:
-            self.attacker=self.monster and self.defender=self.player
+            self.attacker=self.monster
+            self.defander=self.player
 
-    def attack(self):
-        return self.defender.life - self.defender.life
+    def attack(self,attacker,defander):
+        self.attacker=attacker
+        self.defander=defander
+        self.defander.attacked(DAMAGE)
 
-    def check_winner(self):
-        if self.attack()<=0:
-            self.winner = self.attacker.name
-            return self.winner
+    
+    def run(self):
+        self.first_attacker()
+        print(f"Start battle: Attacker: {self.attacker.name} | Defander: {self.defander.name}")
+        self.attack(self.attacker,self.defander)
+        if not self.defander.is_alive():
+            print(f"{self.attacker.name} win!")
 
+        while self.player.is_alive() and self.monster.is_alive():
+            self.attack(self.defander,self.attacker)
+            if not self.defander.is_alive():
+                print(f"{self.attacker.name} win!")
+                break
+            self.attack(self.attacker,self.defander)
+            if not self.defander.is_alive():
+                print(f"{self.attacker.name} win!")
+                break
+
+
+        
         
             
     
--- maneger_game.py
+++ maneger_game.py
@@ -1,21 +1,14 @@
 from player import *
 from config import *
-from maze import *
+from room import *
+from battle import *
 
 
-class Game_manager:
-    def __init__(self):
-        self.player = None
-        self.maze = None
-    def create_player(self):
-        self.player=Player(PLAYER_NAME,PLAYER_LIFE,PLAYER_STRENGH,PLAYER_AGILITY,PLAYER_LUCK)
-    def get_player(self):
-        return self.player
-    def create_maze(self):
-        self.maze=Maze()
-    def get_maze(self):
-        return self.maze
-    def Game_summary():
-        pass
-        
-game=Game_manager()
+class Main:
+    
+    player=Man(PLAYER_NAME,PLAYER_LIFE,PLAYER_STRENGH,PLAYER_AGILITY,PLAYER_LUCK)
+    monster=Monster(MONSTER_NAME,MONSTER_LIFE,MONSTER_STRENGH,MONSTER_AGILITY,MONSTER_LUCK)
+    battle=Battle(player,monster)
+    battle.run()
+
+Main()

e0c1d1ca9b5af34d884c211bbb557900be163d21
Moshe Pfeffer <moishipfeffer@gmail.com>
2026-07-29 10:44:36 +0300

    added

--- room.py
+++ room.py
@@ -1,15 +1,17 @@
 from battle import Battle
-from player import Monster
+from player import Goblin,Orc
 from config import config
 
 
 class Room:
     def __init__(self,player):
         self.player=player
-        self.monster=Monster(config)
+        self.goblin=Goblin(config)
+        self.orc=Orc(config) 
 
     def create_battle(self):
         if isinstance(self.monster,Monster):
             battle=Battle(self.player,self.monster)
             battle.run()
+
 

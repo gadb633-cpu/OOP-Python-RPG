021ebe945dfbdde5d7ad01eb5628ef6c85e89403
Moshe Pfeffer <moishipfeffer@gmail.com>
2026-07-29 09:51:40 +0300

    life 10

--- config.py
+++ config.py
@@ -5,10 +5,10 @@ config={
     "PLAYER AGILITY":5,
     "PLAYER LUCK":3,
     "MONSTER NAME":"monster",
-    "MONSTER LIFE":15,
+    "MONSTER LIFE":10,
     "MONSTER STRENGH":5,
     "MONSTER AGILITY":3,
     "MONSTER LUCK":2,
     "ROOM NUM":1,
-    "DAMAGE":5
+    "DAMAGE":10
 }

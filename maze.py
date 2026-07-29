d06b379eaf2f636194088e24e7180e3ab4e016f4
Moshe Pfeffer <moishipfeffer@gmail.com>
2026-07-29 11:57:57 +0300

    enter and sum more added

--- maze.py
+++ maze.py
@@ -5,8 +5,15 @@ class Maze:
         self.maze = [Room(player,Goblin(config)),Room(player,Orc(config)),Room(player,None),Room(player,None)]
 
     def get_room(self):
-        for room in self.maze:
-            room.create_battle()
+        for i in range(len(self.maze)):
+                enter=input(f"do you want to go into room {i+1}? ")
+                if enter == "yes":
+                    print(f"you are now in room {i+1}")
+                    self.maze[i].create_battle()
+                else:
+                    break
+
+
 
     
    
